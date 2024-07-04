085d825e9acbcc2885cbafdf689be69980d7499c
ogaed <edithoga759@gmail.com>
2024-07-04 17:18:30 +0300

    commit

--- app/controllers/currents_controller.rb
+++ app/controllers/currents_controller.rb
@@ -1,25 +1,7 @@
 class CurrentsController < ApplicationController
-  # before_action :authenticate_user!
-  # def index
-  #   @user = current_user
-  #   render json: { data: @user }
-  # end
-  # def show
-  #   @user = User.find(params[:id])
-  #   render json: { status: 'success', data: @user }
-  # end
-
-  # def index
-  #   @experts = User.where(role: :tourist_attraction).order(average_rating: :desc)
-  #   render json: { status: 'success', data: @experts }
-  # end
+  before_action :authenticate_user!
   def index
-    @users = User.all
-    render json: { status: 'success', data: @users }
+    @user = current_user
+    render json: { data: @user }
   end
-
-  # def show
-  #   @user = User.find(params[:id])
-  #   render json: { status: 'success', data: @user }
-  # end
 end
